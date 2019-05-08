ec6e641202739cb3d50e19bfb8f253fe64565c4d
Malo <malo.polese@hotmail.fr>
2019-05-08 12:38:59 +0200

    changement2

---
+++ KingDomino Android/app/src/main/java/dreamplume/fr/kingdomino/KingStart.java
@@ -0,0 +1,13 @@
+package dreamplume.fr.kingdomino;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KingStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_king_start);
+    }
+}
